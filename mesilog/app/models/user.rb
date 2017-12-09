5667e2b0a9293a629f04773c0f8c83af71731ad7
ryoyaa <33397367+ryoyaa@users.noreply.github.com>
2017-12-10 00:37:54 +0900

    Add files via upload

--- mesilog/app/models/user.rb
+++ mesilog/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+
+  enum :role {
+    nomal_user:0,
+    shop_user:1
+  }
+
   validates :name, presence: true
   validates :email, length: { minimum: 5}
   validates :email, uniqueness: true
